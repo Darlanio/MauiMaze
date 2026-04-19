07aefabf088f102603192b8a4ec73c3cbf648125
Stefan Alenius <darlanio@gmail.com>
2026-04-19 02:56:54 +0200

    Made it controlled by gyro and adjusted speed and friction.

--- MauiMaze/Platforms/Android/MainActivity.cs
+++ MauiMaze/Platforms/Android/MainActivity.cs
@@ -5,14 +5,17 @@ using Android.Views;
 
 namespace MauiMaze
 {
-    [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
+    // ScreenOrientation.Portrait MUST be here:
+    [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true,
+              LaunchMode = LaunchMode.SingleTop,
+              ScreenOrientation = ScreenOrientation.Portrait,
+              ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
     public class MainActivity : MauiAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
 
-            // Hide the Android status bar for full screen
             if (Window != null)
             {
                 Window.SetFlags(WindowManagerFlags.Fullscreen, WindowManagerFlags.Fullscreen);
