a2c235feea13507d4ee10e5de7df06bbc8b47f65
Stefan Alenius <darlanio@gmail.com>
2026-04-26 14:39:05 +0200

    Added Hard Mode

--- MauiMaze/MauiProgram.cs
+++ MauiMaze/MauiProgram.cs
@@ -11,7 +11,7 @@ public static class MauiProgram
         var builder = MauiApp.CreateBuilder();
         builder
             .UseMauiApp<App>()
-            .UseMauiCommunityToolkit() // Satisfies the MCT001 analyzer error
+            .UseMauiCommunityToolkit()
             .ConfigureFonts(fonts =>
             {
                 fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
